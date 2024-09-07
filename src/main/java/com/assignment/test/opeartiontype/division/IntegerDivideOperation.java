6d08138a274830e48f6883dc0a85dc900250e15a
AjayS08 <61771269+AjayS08@users.noreply.github.com>
2024-09-06 21:26:54 -0700

    Update IntegerDivideOperation.java

--- src/main/java/com/assignment/test/opeartiontype/division/IntegerDivideOperation.java
+++ src/main/java/com/assignment/test/opeartiontype/division/IntegerDivideOperation.java
@@ -9,12 +9,11 @@ public class IntegerDivideOperation implements OperationType {
 		if (num2.intValue() == 0) {
 			throw new ArithmeticException("Division by zero is not allowed");
 		}
-		// (-2147483648) / -1 = 2147483648, greater than Integer.MAX_VALUE
+		
 		if (num1.intValue() == Integer.MIN_VALUE && num2.intValue() == -1) {
 			throw new ArithmeticException("Integer overflow in division");
 		}
 
-		// Convert to double to maintain precision in division
 		return num1.doubleValue() / num2.doubleValue();
 	}
 }
